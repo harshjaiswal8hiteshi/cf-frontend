f4c60c0834f38495857217023aefba4e63fe8114
harshjaiswal8hiteshi <harshjaiswal@hiteshi.com>
2025-10-21 00:16:08 +0530

    harsh | updated the config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-module.exports = nextConfig;
+const nextConfig = {
+    basePath: '/cf-frontend',
+    assetPrefix: '/cf-frontend/',
+    trailingSlash: true, // optional, avoids routing issues
+  }
+  
+  module.exports = nextConfig;
+  
