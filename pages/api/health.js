fd4bf021dc21f5e0c306fe1ab43a4b9aa9020bcf
harshjaiswal8hiteshi <harshjaiswal@hiteshi.com>
2025-10-24 17:23:34 +0530

    asdf

--- pages/api/health.js
+++ pages/api/health.js
@@ -4,6 +4,6 @@ export default function handler(req, res) {
     status: 'healthy',
     timestamp: new Date().toISOString(),
     service: 'frontend',
-    version:'8.0'
+    version:'9.0'
   });
 }
